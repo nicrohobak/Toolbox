993ed22d6665a9ac7e86b2c11cbd27e77bf8133d
Nick Robertson <nicrohobak@gmail.com>
2016-03-02 23:57:40 -0800

    Reorganization: Removed an unnecessary directory
    - The Toolbox/Plugin/Common directory has been flattened back into the
      parent directory.  The README and the build_plugins.sh script have
      been updated updated to reflect the change.
    
    - The 'example' directory is ignored by the build_plugins.sh script, but
      all other directories are treated as plugin directories.

---
+++ Plugin/build_plugins.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# Build all common Toolbox plugins
+#
+
+
+CPP=g++
+C_FLAGS="-std=c++14 -Wall -pedantic"
+LD_FLAGS="-fPIC -shared"
+
+PLUGIN_EXT=".so"
+
+
+for PLUGIN_SRC in `for DIR in \`find . -type d | grep -v './example' | grep / | sed 's/^\.\///'\`; do ls $DIR/*.cpp; done`; do
+	PLUGIN_TYPE=`echo $PLUGIN_SRC | sed 's/\/.*$//'`
+	PLUGIN=`echo $PLUGIN_SRC | sed "s/${PLUGIN_TYPE}\///" | sed "s/\.cpp/${PLUGIN_EXT}/"`
+
+	# Make sure the target directory exists
+#	if [ ! -e "${PLUGIN_DIR}/${PLUGIN_TYPE}" ]; then
+#		mkdir -p ${PLUGIN_DIR}/${PLUGIN_TYPE}
+#	fi
+
+	echo "Building ${PLUGIN_TYPE}: ${PLUGIN}..."
+
+	# HACK: Make/use a better build system
+
+	if [ "${PLUGIN}" == "OpenGL.so" ]; then
+		${CPP} ${C_FLAGS} -o ${PLUGIN_TYPE}/${PLUGIN} ${PLUGIN_SRC} ${LD_FLAGS} -lGL
+	elif [ "${PLUGIN}" == "SDL2.so" ]; then
+		${CPP} ${C_FLAGS} -o ${PLUGIN_TYPE}/${PLUGIN} ${PLUGIN_SRC} ${LD_FLAGS} -lSDL2
+	elif [ "${PLUGIN_TYPE}" == "Image" ] && [[ "${PLUGIN}" == "SDL2"* ]]; then
+		${CPP} ${C_FLAGS} -o ${PLUGIN_TYPE}/${PLUGIN} ${PLUGIN_SRC} ${LD_FLAGS} -lSDL2_image
+	elif [ "${PLUGIN_TYPE}" == "Network" ] && [ "`echo "${PLUGIN}" | grep "asio"`" != "" ]; then
+		${CPP} ${C_FLAGS} -o ${PLUGIN_TYPE}/${PLUGIN} ${PLUGIN_SRC} ${LD_FLAGS} -lpthread
+	else
+		${CPP} ${C_FLAGS} -o ${PLUGIN_TYPE}/${PLUGIN} ${PLUGIN_SRC} ${LD_FLAGS}
+	fi
+done
+
+
